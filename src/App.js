3f88192463c27d0456989ea0cce8009ad9535c34
Jared Long <jlong4223@gmail.com>
2021-03-13 12:02:23 -0600

    sets up routes for each page and global page css style

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import { useState, useEffect } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { getUser, logout } from "./services/UserService";
 
+// pages and components
 import "./App.css";
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage";
+import NotFound from "./components/NotFound";
 
 const App = (props) => {
   const [userState, setUserState] = useState({ user: getUser() });
@@ -19,8 +23,14 @@ const App = (props) => {
   }
 
   return (
-    <div className="App">
-      <h1>Hi there</h1>
+    <div className="App page">
+      <header>Hello world</header>
+      <Switch>
+        <Route exact path="/" render={() => <HomePage {...props} />} />
+        <Route exact path="/login" render={() => <LoginPage {...props} />} />
+        <Route component={NotFound} />
+      </Switch>
+      <footer>Footer</footer>
     </div>
   );
 };
