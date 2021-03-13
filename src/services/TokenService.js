c0d36116f73f6c54dd00218218c69367c8c80ad0
Jared Long <jlong4223@gmail.com>
2021-03-12 20:52:19 -0600

    adds userservice and token service

---
+++ src/services/TokenService.js
@@ -0,0 +1,28 @@
+function setToken(token) {
+  token
+    ? localStorage.setItem("token", token)
+    : localStorage.removeItem("token");
+}
+
+function removeToken() {
+  localStorage.removeItem("token");
+}
+
+function getToken(res) {
+  let token = localStorage.getItem("token");
+  if (token) {
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    if (payload.exp < Date.now() / 1000) {
+      localStorage.removeItem("token");
+      token = null;
+    }
+  }
+  return token;
+}
+
+function getUserFromToken() {
+  const token = getToken();
+  return token ? JSON.parse(atob(token.split(".")[1])).user : null;
+}
+
+export { setToken, getToken, getUserFromToken, removeToken };
---
+++ src/services/UserService.js
@@ -0,0 +1,44 @@
+import { setToken, getUserFromToken, removeToken } from "./TokenService";
+
+const BASE_URL = "http://localhost:3001/users/";
+
+function signup(user) {
+  return fetch(BASE_URL + "signup", {
+    method: "POST",
+    headers: new Headers({ "Content-Type": "application/json" }),
+    body: JSON.stringify(user),
+  })
+    .then((res) => {
+      if (res.ok) return res.json();
+      // Probably a duplicate email
+      throw new Error("Email already taken!");
+    })
+    .then(({ token }) => setToken(token));
+}
+
+function login(credentials) {
+  return fetch(BASE_URL + "login", {
+    method: "POST",
+    headers: new Headers({ "Content-Type": "application/json" }),
+    body: JSON.stringify(credentials),
+  })
+    .then((res) => {
+      if (res.ok) return res.json();
+      // Probably a duplicate email
+      else {
+        alert("Bad Credentials");
+        throw new Error("Bad credentials");
+      }
+    })
+    .then(({ token }) => setToken(token));
+}
+
+function getUser() {
+  return getUserFromToken();
+}
+
+function logout() {
+  return removeToken();
+}
+
+export { signup, getUser, logout, login };
