22d238bf9f84e824c29deb8e9aeff69d047b8f30
Jared Long <jlong4223@gmail.com>
2021-03-12 20:52:46 -0600

    removes default react styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { getUser, logout } from "./services/UserService";
 
-function App() {
+import "./App.css";
+
+const App = (props) => {
+  const [userState, setUserState] = useState({ user: getUser() });
+
+  function handleRegisterOrLogin() {
+    setUserState({ user: getUser() });
+    props.history.push("/");
+  }
+
+  function handleLogout() {
+    logout();
+    setUserState({ user: null });
+    props.history.push("/");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi there</h1>
     </div>
   );
-}
+};
 
 export default App;
