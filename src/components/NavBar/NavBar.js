2d3e7cafa5c0ea5602e77e1e10ebfacf06d917c6
Jared Long <jlong4223@gmail.com>
2021-03-14 10:53:51 -0500

    sets up navbar with placeholeder links

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+const NavBar = (props) => {
+  return (
+    <header id="navHeader">
+      <Link className="navLink" to="">
+        ABOUT US
+      </Link>
+      <Link className="navLink" to="">
+        ADOPT A PET
+      </Link>
+      <Link className="navLink" to="">
+        CONTACT US
+      </Link>
+      <Link className="navLink" to="">
+        RESOURCES
+      </Link>
+    </header>
+  );
+};
+
+export default NavBar;
