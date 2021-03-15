2d9cd1b0b16a54a6ec78b91d0a2bac319474225d
Jared Long <jlong4223@gmail.com>
2021-03-15 10:48:29 -0500

    fixes sticky header issues with updated css class

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -12,10 +12,11 @@ import {
 import "./NavBar.css";
 
 const NavBar = (props) => {
-  // TODO look into sticky header issue
+  // this state is for the dropdown toggle
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const toggle = () => setDropdownOpen(!dropdownOpen);
 
+  // sticky header state
   const [isSticky, setSticky] = useState(false);
   const ref = useRef(null);
   const handleScroll = () => {
@@ -30,60 +31,58 @@ const NavBar = (props) => {
     };
   }, []);
   return (
-    <header
-      id="navHeader"
-      className={`sticky-wrapper${isSticky ? " sticky" : ""}`}
-      ref={ref}
-    >
-      <Link className="navLink" to="">
-        ABOUT US
-      </Link>
+    <header className={`sticky-wrapper${isSticky ? " sticky" : ""}`} ref={ref}>
+      <header id="innerHeader" className="sticky-inner">
+        <Link className="navLink" to="">
+          ABOUT US
+        </Link>
 
-      {/* reactstate drop down stuff */}
-      <Nav pills>
-        <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
-          <DropdownToggle nav caret className="navLink">
-            ADOPT A PET
-          </DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem className="drop">
-              <Link to="/adopt">Adoption Form</Link>
-            </DropdownItem>
-            <DropdownItem divider />
-            {props.user && props.user.admin === true ? (
+        {/* reactstate drop down components */}
+        <Nav pills>
+          <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
+            <DropdownToggle nav caret className="navLink">
+              ADOPT A PET
+            </DropdownToggle>
+            <DropdownMenu>
               <DropdownItem className="drop">
-                <Link to="/addpet">Action</Link>
+                <Link to="/adopt">Adoption Form</Link>
               </DropdownItem>
-            ) : (
-              <DropdownItem className="drop" disabled>
-                Action
+              <DropdownItem divider />
+              {props.user && props.user.admin === true ? (
+                <DropdownItem className="drop">
+                  <Link to="/addpet">Action</Link>
+                </DropdownItem>
+              ) : (
+                <DropdownItem className="drop" disabled>
+                  Action
+                </DropdownItem>
+              )}
+              <DropdownItem divider />
+              <DropdownItem className="drop">
+                <Link to="/pets">Pets</Link>
               </DropdownItem>
-            )}
-            <DropdownItem divider />
-            <DropdownItem className="drop">
-              <Link to="/pets">Pets</Link>
-            </DropdownItem>
-            <DropdownItem divider />
-            <DropdownItem className="drop">
-              <Link to="/prices">Prices</Link>
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-        <NavItem>
-          <NavLink href="#"></NavLink>
-        </NavItem>
-        {/* <NavItem>
+              <DropdownItem divider />
+              <DropdownItem className="drop">
+                <Link to="/prices">Prices</Link>
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+          <NavItem>
+            <NavLink href="#"></NavLink>
+          </NavItem>
+          {/* <NavItem>
           <NavLink disabled href="#">
             Disabled Link
           </NavLink>
         </NavItem> */}
-      </Nav>
-      <Link className="navLink" to="/contact">
-        CONTACT US
-      </Link>
-      <Link className="navLink" to="">
-        RESOURCES
-      </Link>
+        </Nav>
+        <Link className="navLink" to="/contact">
+          CONTACT US
+        </Link>
+        <Link className="navLink" to="">
+          RESOURCES
+        </Link>
+      </header>
     </header>
   );
 };
