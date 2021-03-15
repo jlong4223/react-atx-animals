8b1bd22f2bdea243f2c2c7a150b2b46764f8f3dc
Jared Long <jlong4223@gmail.com>
2021-03-15 15:10:15 -0500

    changes sizing of the google form

--- src/components/AdoptForm.js
+++ src/components/AdoptForm.js
@@ -4,10 +4,10 @@ const AdoptForm = () => {
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLScDk-mwZQaYrJJqs3Oysw-TrOlf-cPTXt8BZVnuwjnD336EeQ/viewform?embedded=true"
         width="800"
-        height="1644"
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
+        height="550"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
       >
         Loading…
       </iframe>
