abc94221960c64110b1ce2974a1fa8b82cc7873e
Jared Long <jlong4223@gmail.com>
2021-03-15 23:21:21 -0500

    adds tasks page component for admin

---
+++ src/components/Tasks/Tasks.js
@@ -0,0 +1,12 @@
+const Tasks = (props) => {
+  return (
+    <div>
+      <h1>Tasks here</h1>
+      <h2>Open Tasks</h2>
+      <h3>Pending Tasks</h3>
+      <h3>Closed Tasks</h3>
+    </div>
+  );
+};
+
+export default Tasks;
