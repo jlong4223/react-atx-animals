cfad95baa8e7fb21807449bb7afde22c92e2634a
Jared Long <jlong4223@gmail.com>
2021-03-19 14:32:58 -0500

    imports home contents component into home page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,39 @@
+import HomeContents from "../../components/HomeContents/HomeContents";
+import images from "../../components/HomeContents/HomeImages";
+
 export default function HomePage() {
+  let style = {
+    width: "100%",
+    height: "75vh",
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${images[1]})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    display: "flex",
+    justifyContent: "center",
+  };
+
   return (
-    <main className="page" id="homepage">
-      <div>
-        <h1>HomePage</h1>
-      </div>
-    </main>
+    <>
+      <main className="page" style={style}>
+        <div style={{ width: "60%" }}>
+          <h1
+            style={{
+              color: "white",
+              textAlign: "center",
+              paddingBottom: "35px",
+            }}
+          >
+            ADOPT AND GET BACK MORE THAN WHAT’S HUMANLY POSSIBLE
+            <br />
+            <a href="/#link" style={{ color: "white" }}>
+              <i class="fas fa-arrow-circle-down"></i>
+            </a>
+          </h1>
+        </div>
+      </main>
+      <main className="page">
+        <HomeContents />
+      </main>
+    </>
   );
 }
