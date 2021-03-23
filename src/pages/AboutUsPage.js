64cdc7754ffe1fa47a29073fa33762135a842612
Jared Long <jlong4223@gmail.com>
2021-03-23 12:52:48 -0500

    creates new routes and about us page with styles

---
+++ src/pages/AboutUsPage.js
@@ -0,0 +1,11 @@
+import AboutUs from "../components/AboutUs";
+
+const AboutUsPage = () => {
+  return (
+    <main className="page" style={{ backgroundColor: "whitesmoke" }}>
+      <AboutUs />
+    </main>
+  );
+};
+
+export default AboutUsPage;
